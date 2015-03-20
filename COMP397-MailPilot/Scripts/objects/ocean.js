ebf499cef3501e55b902150b5eae5c7252bed0c5
srichatala <sri2vas@gmail.com>
2015-03-20 15:03:19 -0400

    MailPilot game is commited

---
+++ COMP397-MailPilot/Scripts/objects/ocean.js
@@ -0,0 +1,40 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var objects;
+(function (objects) {
+    // OCEAN CLASS
+    var Ocean = (function (_super) {
+        __extends(Ocean, _super);
+        // CONSTRUCTOR
+        function Ocean() {
+            _super.call(this, assetLoader.getResult("ocean"));
+            // PUBLIC INSTANCE VARIABLES
+            this._dy = 5;
+            this.reset();
+        }
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+        Ocean.prototype.update = function () {
+            this.y += this._dy;
+            this._checkBounds();
+        };
+        // Reset position of island to the top
+        Ocean.prototype.reset = function () {
+            this.y = -960;
+            this.x = 0;
+        };
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++
+        Ocean.prototype._checkBounds = function () {
+            // check if island has left the bottom of the screen
+            if (this.y === 0) {
+                this.reset();
+            }
+        };
+        return Ocean;
+    })(createjs.Bitmap);
+    objects.Ocean = Ocean;
+})(objects || (objects = {}));
+//# sourceMappingURL=ocean.js.map
